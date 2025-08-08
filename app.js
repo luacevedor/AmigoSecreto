166acf5135d72049a7ecf04a236800ddf4e4141a
Luis <luis.gacevedor@gmail.com>
2025-08-08 02:09:58 -0500

    Agrega archivo de app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let MisAmigos = ["Ana Maria", "Juan Pablo"; "Luz Dora", "Aelyn Sofia"; "Maria Teresa"];
+let MisAmigos = ["Ana Maria", "Juan Pablo"; "Luz Dora", "Aelyn Sofia"];
+
