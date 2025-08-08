de14fc460be0282f910501fd29c378128792612e
Luis <luis.gacevedor@gmail.com>
2025-08-08 01:53:02 -0500

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let MisAmigos = ["Ana Maria", "Juan Pablo"; "Luz Dora", "Aelyn Sofia"];
