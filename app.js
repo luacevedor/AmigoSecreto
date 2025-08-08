c43fbfca763e01941d3d41ad065861d090eb385b
Luis <luis.gacevedor@gmail.com>
2025-08-08 14:16:23 -0500

    Agregar archivo de app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 
 
 let misAmigos = [];
 
 console.log (misAmigos);
-
 
 function agregarAmigo() {
 let nombresDeAmigos = document.getElementById('amigo').value;
