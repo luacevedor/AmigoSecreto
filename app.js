28274ba8d4fe15f62808055ed8b12ce0c47f1ea5
Luis <luis.gacevedor@gmail.com>
2025-08-08 13:20:11 -0500

    Agregar archivo de app.js

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ console.log (misAmigos);
 function agregarAmigo() {
 let nombresDeAmigos = document.getElementById('amigo').value;
 
+if (nombresDeAmigos === "") {
+
+    alert ('Ingrese Nombre de Amigo');
+    return;
+}
+
+misAmigos.push(agregarAmigo);
+actualizarAmigos();
+document.getElementById('amigo').value = "";
+
 }
